b7256ec159f43fe83a0f84d2117c73d4422dc9d2
satyabrat padhy <satyadeui@gmail.com>
2020-10-31 01:31:51 -0700

    comments done

--- src/app/users-blog/posts-details/posts-details.component.ts
+++ src/app/users-blog/posts-details/posts-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
-import { Observable, Subject } from 'rxjs';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 import { UsersblogService } from 'src/app/services/usersblog.service';
 
 @Component({
@@ -13,7 +13,10 @@ export class PostsDetailsComponent implements OnInit {
   comments:Observable<any>;
   showLoadingIndicator = true;
   toggleSpinner:boolean = false;
-  castCmnt = new Subject();
+  cmntsSubscribe = new BehaviorSubject({});
+  getCmntsId(id){
+    this.cmntsSubscribe.next(id);
+  }
   constructor(private _route:ActivatedRoute,private _router:Router,private usersBlogService:UsersblogService) { 
     this.postDetails = this._route.snapshot.data['postDetails'];
     console.log('posts:', this.postDetails);
@@ -30,10 +33,15 @@ export class PostsDetailsComponent implements OnInit {
   }
   
   ngOnInit(): void {
+    this.cmntsSubscribe.subscribe(
+      (id:any)=>{
+        this.comments=this.usersBlogService.getCommentsById(id);
+      }
+    );
   }
   showComments(id){
     this.toggleSpinner = !this.toggleSpinner;
-    this.comments=this.usersBlogService.getCommentsById(id);
+    this.getCmntsId(id);
   }
   delPost(id,userId){
     this.usersBlogService.delPostById(id).subscribe(
